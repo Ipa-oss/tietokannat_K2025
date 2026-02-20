7e352a1a8a4fb393ec4dacd3959966cf654d6658
Ipa-oss <backman.ilari@proton.me>
2026-02-20 11:43:43 +0200

    Lisätty tehtävä 17.

---
+++ Viikko6/callback_example.js
@@ -0,0 +1,19 @@
+//Tehtava17.1.
+/*setTimeout(doSomething,2000);
+
+function doSomething(){
+    console.log("Demonstrating the callbacks");
+}
+console.log("The application is started");*/
+
+//Tehtava17.2.
+/*const doSomething = function() {
+    console.log("Demonstrating the callbacks");
+}
+setTimeout(doSomething,2000);
+console.log("The application is started");*/
+
+//Tehtava17.3.
+/*const doSomething = () => console.log("Demonstrating the callbacks");
+setTimeout(doSomething,2000);
+console.log("The application is started");*/
